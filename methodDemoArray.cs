1e2f60ce3755a35c6ff7c2328a308a8f7ab3972d
Prak22 <94758715+Prak22@users.noreply.github.com>
2022-10-27 20:31:42 +0530

    Add files via upload

---
+++ methodDemoArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace methodDemoArray
+{
+    class methodDemoArray
+    {
+        
+        public void printArray(int[] arr)
+        {
+
+            Console.WriteLine("Printing array elements:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+
+        }
+         static void Main(string[] args)
+        {
+
+            methodDemoArray obj = new methodDemoArray();
+            int[] arr = { 1, 2, 3, 4, 5 };
+            obj.printArray(arr);//passing array to function  
+            
+        }
+    }
+}
